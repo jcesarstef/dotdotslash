417489c8d3b8e85a39232771e8c8aff1a222307a
Julio C. Stefanutto <jcesarstef@gmail.com>
2018-03-09 23:36:55 -0300

    false positive fix

--- match.py
+++ match.py
@@ -76,7 +76,7 @@ match = {
 "c:\windows\system32\drivers\hosts": "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t]+[a-zA-Z0-9-_.]*",
 
 # Linux
-"etc/hosts": "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\W+[a-zA-Z0-9-_.]*",
+"etc/hosts": "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t][a-zA-Z0-9-_.]*",
 "etc/passwd": "\w*\:\w\:[0-9]*\:[0-9]*\:[a-zA-Z_-]*\:[\/a-zA-Z0-9]*[ \t]+:[\/a-zA-Z0-9]*",
 # TODO
 #"etc/group": "\w+\:\w\:[0-9]\:(|[a-z,]*)",

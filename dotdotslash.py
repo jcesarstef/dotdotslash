cb1c12552665c50f6f9c981e2b91373da4af2c44
Julio C. Stefanutto <jcesarstef@gmail.com>
2018-04-02 11:57:53 -0300

    Update dotdotslash.py

--- dotdotslash.py
+++ dotdotslash.py
@@ -6,42 +6,6 @@ from match import dotvar, match, befvar
 import requests
 from http.cookies import SimpleCookie
 
-# TODO
-# add -f --file
-# add --os
-# http://site.com.br/some-page?page=http://other-site.com.br/
-# use POST data
-# mutiple headers
-# solve 302 that turn 200
-# save in sqlite/csv
-# colors on http_code
-# threads
-
-parser = argparse.ArgumentParser(description='dot dot slash - A automated Path Traversal Tester. Created by @jcesrstef.')
-parser.add_argument('--url', '-u', action='store', dest='url', required=True, help='Url to attack.')
-parser.add_argument('--string', '-s', action='store', dest='string', required=True, help='String in --url to attack. Ex: document.pdf')
-parser.add_argument('--cookie', '-c', action='store', dest='cookie', required=False, help='Document cookie.')
-parser.add_argument('--depth', '-d', action='store', dest='depth', required=False, type=int, default='6', help='How deep we will go?')
-parser.add_argument('--verbose', '-v', action='store_true', required=False, help='Show requests')
-arguments = parser.parse_args()
-
-banner = "\
-     _       _         _       _         _           _     \n\
-  __| | ___ | |_    __| | ___ | |_   ___| | __ _ ___| |__  \n\
- / _` |/ _ \| __|  / _` |/ _ \| __| / __| |/ _` / __| '_ \ \n\
-| (_| | (_) | |_  | (_| | (_) | |_  \__ \ | (_| \__ \ | | |\n\
- \__,_|\___/ \__|  \__,_|\___/ \__| |___/_|\__,_|___/_| |_|\n\
-                                                           \n\
-Automated Path Traversal Tester\n\
-version 0.0.2\n\
-Created by Julio Cesar Stefanutto (@jcesarstef)\n\
-\n\
-Starting run in: \033[94m" + arguments.url + "\033[0m\n\
-\
-"
-print(banner)
-
-
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -51,6 +15,22 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+def codecollors(code):
+    if str(code).startswith("2"):
+        colorized = "\033[92m[" + str(code) + "] \033[0m"
+        return colorized
+    elif str(code).startswith("3"):
+        colorized = "\033[93m[" + str(code) + "] \033[0m"
+        return colorized
+    elif str(code).startswith("4"):
+        colorized = "\033[91m[" + str(code) + "] \033[0m"
+        return colorized
+    elif str(code).startswith("5"):
+        colorized = "\033[94m[" + str(code) + "] \033[0m"
+        return colorized
+    else:
+        return code
 
 
 class request(object):
@@ -84,11 +64,12 @@ def forloop():
                         req.query(fullrewrite)
                         catchdata = re.findall(str(match[word]), req.raw)
                         if (len(catchdata) != 0):
-                            print(bcolors.OKGREEN + "\n[" + str(req.code) + "] " + bcolors.ENDC + fullrewrite)
+                            #print(bcolors.OKGREEN + "\n[" + str(req.code) + "] " + bcolors.ENDC + fullrewrite)
+                            print(codecollors(req.code) + fullrewrite)
                             print(" Contents Found: " + str(len(catchdata)))
                         else:
                             if arguments.verbose:
-                                print("[" + str(req.code) + "] " + fullrewrite)
+                                print(codecollors(req.code) + fullrewrite)
 
                         icount = 0
                         # Print match
@@ -103,4 +84,31 @@ def forloop():
                     duplicate.append(fullrewrite)
         count += 1
 
-forloop()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='dot dot slash - A automated Path Traversal Tester. Created by @jcesrstef.')
+    parser.add_argument('--url', '-u', action='store', dest='url', required=True, help='Url to attack.')
+    parser.add_argument('--string', '-s', action='store', dest='string', required=True, help='String in --url to attack. Ex: document.pdf')
+    parser.add_argument('--cookie', '-c', action='store', dest='cookie', required=False, help='Document cookie.')
+    parser.add_argument('--depth', '-d', action='store', dest='depth', required=False, type=int, default='6', help='How deep we will go?')
+    parser.add_argument('--verbose', '-v', action='store_true', required=False, help='Show requests')
+    arguments = parser.parse_args()
+
+    banner = "\
+         _       _         _       _         _           _     \n\
+      __| | ___ | |_    __| | ___ | |_   ___| | __ _ ___| |__  \n\
+     / _` |/ _ \| __|  / _` |/ _ \| __| / __| |/ _` / __| '_ \ \n\
+    | (_| | (_) | |_  | (_| | (_) | |_  \__ \ | (_| \__ \ | | |\n\
+     \__,_|\___/ \__|  \__,_|\___/ \__| |___/_|\__,_|___/_| |_|\n\
+                                                           \n\
+    Automated Path Traversal Tester\n\
+    version 0.0.9\n\
+    Created by Julio Cesar Stefanutto (@jcesarstef)\n\
+    \n\
+    Starting run in: \033[94m" + arguments.url + "\033[0m\n\
+    \
+    "
+    print(banner)
+    forloop()
+
